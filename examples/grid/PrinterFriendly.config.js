6ff9003a8b83d06c41af74424da10a3f925a74de
Steffen Hiller <s.hiller@caprireleases.com>
2010-09-17 10:31:25 -0430

    Added grid and grouping grid examples. They are using ext js 2.3.0 now. Before it was 2.2.1.

---
+++ examples/grid/PrinterFriendly.config.js
@@ -0,0 +1,27 @@
+/*  Ext.ux.PrinterFriendly
+ *  (c) 2008 Steffen Hiller (http://www.extjswithrails.com)
+ *
+ *  License: Ext.ux.PrinterFriendly is licensed under the terms of
+ *  the Open Source LGPL 3.0 license.  Commercial use is permitted to the extent
+ *  that the code/component(s) do NOT become part of another Open Source or Commercially
+ *  licensed development library or toolkit without explicit permission.
+ *
+ *  License details: http://www.gnu.org/licenses/lgpl.html
+ *
+ *  This is an extension for the Ext JS Library, for more information see http://www.extjs.com.
+ *--------------------------------------------------------------------------*/
+
+// Ext.namespace('Ext.ux.PrinterFriendly');
+Ext.ux.PrinterFriendly = function(){};
+
+/*  
+ * The path to your ux.PrinterFriendly folder
+ */
+Ext.ux.PrinterFriendly.ROOT = '../../source';
+
+/*  
+ * Instead of using Ext.ux.PrinterFriendly.onReady and Ext.ux.PrinterFriendly.onPrinting
+ * you can use Ext.onReady and Ext.onPrinting
+ * See Shortcuts.js for all available shortcuts
+ */
+Ext.ux.PrinterFriendly.ENABLE_SHORTCUTS = true;
